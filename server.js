2d6bcf10a825f38818ea6eea6ab388232d2dc0c0
Afolabi Marvelous <afosmarvel@gmail.com>
2020-01-29 10:08:32 -0800

    some changes made

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express') ;
 
 const app = express() ;
 
+// connect project to front end
+
 app.use(express.static(__dirname + '/client')) ;
 
 // We set up environment variable
 
 const port = process.env.PORT || 3000 ;
+
+// Check if project is connected to the server 
 
 app.listen(port, () => {
 	console.log('port successfully running on port ' + port) ;
